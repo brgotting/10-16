ad62ba120f6fb1ef86ded53ae6fcefe5d4814bbf
Blake Gotting and Cole Marsteller <blake@>
2018-10-16 11:03:53 -0700

    add color changing buttons, hide preious work

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,46 @@
 $(document).ready(function() {
-  // $(".clickable").click(function() {
-  //   $("#walrus-showing").toggle();
-  //   $("#walrus-hidden").toggle();
-  // });
 
-$(".slide").click(function(){
-    $("#walrus-showing-slide").slideToggle();
-    $("#walrus-hidden-slide").slideToggle();
-    // $("#walrus-showing").fadeToggle("slow");
-    // $("#walrus-showing").fadeToggle(3000);
-});
-
-$(".fade").click(function(){
-    $("#walrus-showing-fade").fadeToggle();
-    $("#walrus-hidden-fade").fadeToggle();
-    // $("#walrus-showing").fadeToggle("slow");
-    // $("#walrus-showing").fadeToggle(3000);
-});
-
-// $(".clickable").click(function(){
-//     $("#walrus-hidden").fadeOut();
-//     $("#walrus-hidden").fadeOut("slow");
-//     $("#walrus-hidden").fadeOut(3000);
+// $(".slide").click(function(){
+//     $("#walrus-showing-slide").slideToggle();
+//     $("#walrus-hidden-slide").slideToggle();
+//
 // });
+//
+// $(".fade").click(function(){
+//     $("#walrus-showing-fade").fadeToggle();
+//     $("#walrus-hidden-fade").fadeToggle();
+//
+// });
+
+
+
+$(document).ready(function() {
+  $("button#green").click(function() {
+    $("body").removeClass();
+    $("body").addClass("green-background");
+  });
+
+  $("button#yellow").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
+
+  });
+
+  $("button#red").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
 
 });
